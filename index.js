62ab2f4bfd81fadf0d4df725150e07264356625f
Nathan Elg <elgn@students.wwwu.edu>
2017-05-08 13:57:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var server = express()
+var port = 3000
+
+
+var books = [{
+  id: 1,
+  title: "Harry Potter",
+  published: "2003",
+  rating: "5 Bubbles",
+  Author: "JK Rowling"
+},{
+   id: 2,
+  title: "Harry Potter",
+  published: "2003",
+  rating: "5 Bubbles",
+  Author: "JK Rowling"
+},{
+   id: 3,
+  title: "Harry Potter",
+  published: "2003",
+  rating: "5 Bubbles",
+  Author: "JK Rowling"
+},{
+   id: 4,
+  title: "Harry Potter",
+  published: "2003",
+  rating: "5 Bubbles",
+  Author: "JK Rowling"
+}
+
+]
